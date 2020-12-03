bcb3364866b222963208ba9358dd054fbd0fcc2b
MilanTuryna <miloslavturyna@seznam.cz>
2020-12-03 15:25:37 +0100

    Added comment to some statements in server.ts

--- api/src/server.ts
+++ api/src/server.ts
@@ -4,16 +4,16 @@ import {BotManager} from "./model/minecraft/bots/BotManager";
 import * as express from "express";
 
 const app = express();
-const storagePath = __dirname + '/../data';
+const storagePath = __dirname + '/../data'; // Folder for storage saved bot configurations
 const botManager = new BotManager(new FileReader(storagePath));
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-app.set('json spaces', 2);
-app.get('/', (req, res) => res.send("hiiii"));
+app.set('json spaces', 2); // pretty print json
+app.get('/', (req, res) => res.redirect("https://github.com/MilanTuryna/virtualplayers", 301));
 app.get('/bots', (req, res) => {
     const options: {} = {
         count: Object.keys(botManager.getBotsList()).length,
@@ -22,8 +22,8 @@ app.get('/bots', (req, res) => {
     res.json(options);
 });
 app.get('/bots/:nickname/instance', function (req, res) {
-        let botName = req.params.nickname;
         try {
+            let botName = req.params.nickname;
             let botInstance = botManager.getBotInstance(botName);
             let minecraftBot = botInstance.minecraftBot;
             let responseJson: {} = {
